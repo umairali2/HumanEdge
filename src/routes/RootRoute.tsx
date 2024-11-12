9ade37feb0206977e6083488c00e7d250aea9400
umairali2 <sayedumairali@gmail.com>
2024-11-12 15:54:21 +0500

    routing added

--- src/routes/RootRoute.tsx
+++ src/routes/RootRoute.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Feedback from "../pages/Feedback";
 
 type Props = {};
 
@@ -7,9 +8,9 @@ export const RootRoute = (props: Props) => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route index element={<h1>Home 1</h1>} />
-        <Route path="blogs" element={<h1>Blogs</h1>} />
+        <Route path="/" element={<Feedback />} />
+        <Route index element={<Feedback />} />
+        <Route path="/feedback" element={<Feedback />} />
         <Route path="contact" element={<h1>Contact</h1>} />
         <Route path="*" element={<h1>NoPage</h1>} />
       </Routes>
